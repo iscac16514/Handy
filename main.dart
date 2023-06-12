19bfae338dad112603de5b5a488b485f183c34b5
iscac16514 <127222750+iscac16514@users.noreply.github.com>
2023-06-12 17:44:23 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:handy/firebase_options.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() async {
+  await GetStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
